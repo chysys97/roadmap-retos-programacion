60632a5e34bdb0d47e752ff360f3900129a3da35
chysys97 <christiandiazst@gmail.com>
2025-09-25 15:36:04 +0200

    Create chrysys.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/chrysys.py
@@ -0,0 +1,27 @@
+# https://www.python.org
+
+# Comentario en una linea
+
+"""
+Esto tambien es
+un comentario en varias lineas
+"""
+
+'''
+esto tambien 
+es un comentario 
+en varias lineas
+'''
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT ="Mi constante" # por convencion
+
+my_int = 5
+my_float = 5.5
+my_bool = True
+my_bool = False
+my_str = "Mi cadena de texto"
+my_other_trs = "Mi otra cadena de texto"
+
+print("¡Hola, Python!) 
